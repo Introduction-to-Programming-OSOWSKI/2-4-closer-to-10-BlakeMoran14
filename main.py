0e5a41768de9685972db304a0b3e2b1aed676407
BlakeMoran14 <818523@mystma.org>
2021-09-28 14:56:09 -0500

    Closer to 10

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #WRITE YOUR CODE IN THIS FILE
+
+def close10(x, y):
+    xa = abs(x - 10)
+    ya = abs(y - 10)
+
+    if xa > ya:
+        return y
+    
+    elif xa < ya:
+        return x
+
+    else:
+        return 0
